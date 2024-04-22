b67f41c70a7cbc4d184a3fa9e65fb508179ee16a
alexacerto <alexacerto@ufl.edu>
2024-04-22 13:46:12 -0400

    code changes

--- GeoForecast/app/models/temperature_record.rb
+++ GeoForecast/app/models/temperature_record.rb
@@ -16,76 +16,105 @@ class TemperatureRecord < ApplicationRecord
   
   # max_radius set to furthest point on continental US from data
   def self.find_nearby_records(latitude, longitude, initial_radius = 2000, increment_step = 1000, max_radius = 2000000, datetime = DateTime.now, sort_algo = 1)
-    location_point = RGeo::Geographic.spherical_factory(srid: 4326).point(longitude, latitude)
-    radius = initial_radius
-    records = where("ST_DWithin(location, ST_SetSRID(ST_MakePoint(?, ?), 4326), ?)", longitude, latitude, radius)
-    # Flag to check if radius was ever incremented
-    radius_incremented = false 
-    
-    while records.none? && radius <= max_radius
-      puts "Radius: " + radius.to_s
-      radius += increment_step
-      radius_incremented = true 
+      location_point = RGeo::Geographic.spherical_factory(srid: 4326).point(longitude, latitude)
+      radius = initial_radius
       records = where("ST_DWithin(location, ST_SetSRID(ST_MakePoint(?, ?), 4326), ?)", longitude, latitude, radius)
-      puts "Found: " + records.count.to_s + " records"
+                .order(Arel.sql("ST_Distance(location, ST_SetSRID(ST_MakePoint(#{longitude}, #{latitude}), 4326))"))
+      # Flag to check if radius was ever incremented
+      radius_incremented = false
 
-    end
+      while records.none? && radius <= max_radius
+        puts "Radius: " + radius.to_s
+        radius += increment_step
+        radius_incremented = true
+        records = where("ST_DWithin(location, ST_SetSRID(ST_MakePoint(?, ?), 4326), ?)", longitude, latitude, radius)
+                .order(Arel.sql("ST_Distance(location, ST_SetSRID(ST_MakePoint(#{longitude}, #{latitude}), 4326))"))
+        puts "Found: " + records.count.to_s + " records"
 
-    # If records are found after incrementing, and radius can be further expanded by 2000 meters, do so
-    if records.any? && radius_incremented && (radius + 2000) <= max_radius
-      radius += 2000
-      records = where("ST_DWithin(location, ST_SetSRID(ST_MakePoint(?, ?), 4326), ?)", longitude, latitude, radius)
-      puts "Found additional: " + records.count.to_s + " records"
+      end
 
-    end
+      # If records are found after incrementing, and radius can be further expanded by 2000 meters, do so
+      if records.any? && radius_incremented && (radius + 2000) <= max_radius
+        radius += 2000
+        records = where("ST_DWithin(location, ST_SetSRID(ST_MakePoint(?, ?), 4326), ?)", longitude, latitude, radius)
+                .order(Arel.sql("ST_Distance(location, ST_SetSRID(ST_MakePoint(#{longitude}, #{latitude}), 4326))"))
+        puts "Found additional: " + records.count.to_s + " records"
+
+      end
+
 
     # TODO: call sorting function(s) here, provide records as input
 
       # use user-pvoided flag to determine which sorting algorithm to use
       # if sort_algo = 0
-        # sorted_records = sort_merge(records, datetime)
+      # sorted_records = merge_sort(records)
       # elif sort_algo = 1
         # sorted_records = sort_heap(records, datetime)
       # else
         # sorted_records = run both
 
     # filtered_records = self.filter_records(sorted_records, datetime)
-    return
 
-    records
+    records.to_a
+    # merge_sort(records.to_a)
     # filtered_records
-  end
+   end
+#
+#   # NOTE: records may already be sorted by date, verify this and randomize prior to sorting if necessary
+#
+#   # TODO: implement sorting algorithm 1
 
-  # NOTE: records may already be sorted by date, verify this and randomize prior to sorting if necessary
+    def self.merge_sort(records)
+      puts "Sorting #{records.length} records..."  # Debug output
+      return records if records.length <= 1
 
-  # TODO: implement sorting algorithm 1
-  def self.sort_merge(records, datetime) {
+      mid = records.length / 2
+      left_sorted = merge_sort(records[0...mid])
+      right_sorted = merge_sort(records[mid..])
 
-  }
-
-  # TODO: imeplement sorting algorithm 2
-  def self.sort_heap(records, datetime) {
-
-  }
-
-  # TODO: implement sorting algorithm performance tracking
-
-  #TODO: filter sorted records:
-  def self.filter_records(records, datetime) {
-    # first find all records within one month
-
-    # find all records within 2 hours of time and 1 month of date
-      # if no records are within 1 month, ignore date and just filter by time
-      # if no records are within 2 hours, increment by 1 hours until increment is 6 hours
-        # if no records found within 6 hours, filter by date and increment by 1 month until records found
-  }
-
-  def self.filter_by_date(records, date, range = 1.month, increment = false) {
-    # find all records within the range of the date (ignore time)
-    filtered_records = records.select { |record| record.datetime.to_date >= date.to_date && record.datetime.to_date <= (date + range).to_date }
-    # if filtered_records 
+      merge(left_sorted, right_sorted)
+    end
 
 
-    # if range = 12.months, return records
-  }
+    def merge(left, right)
+      sorted = []
+      while !left.empty? && !right.empty?
+        # Compare the datetime attributes of the left and right elements
+        if left.first.datetime <= right.first.datetime
+          sorted << left.shift
+        else
+          sorted << right.shift
+        end
+      end
+
+      # Concatenate what's left in either left or right arrays to the sorted array
+      sorted.concat(left).concat(right)
+      sorted
+    end
+#
+#   # TODO: imeplement sorting algorithm 2
+#   def self.sort_heap(records) {
+#
+#   }
+#
+#   # TODO: implement sorting algorithm performance tracking
+#
+#   #TODO: filter sorted records:
+#   def self.filter_records(records, datetime) {
+#     # first find all records within one month
+#
+#     # find all records within 2 hours of time and 1 month of date
+#       # if no records are within 1 month, ignore date and just filter by time
+#       # if no records are within 2 hours, increment by 1 hours until increment is 6 hours
+#         # if no records found within 6 hours, filter by date and increment by 1 month until records found
+#   }
+#
+#   def self.filter_by_date(records, date, range = 1.month, increment = false) {
+#     # find all records within the range of the date (ignore time)
+#     filtered_records = records.select { |record| record.datetime.to_date >= date.to_date && record.datetime.to_date <= (date + range).to_date }
+#     # if filtered_records
+#
+#
+#     # if range = 12.months, return records
+#   }
 end
--- GeoForecast/lib/tasks/import.rake
+++ GeoForecast/lib/tasks/import.rake
@@ -1,5 +1,6 @@
 # PLACE CSV TO BE IMPORTED RIGHT OUTSIDE OF THE 'GeoForecast' FOLDER
-# Run command: bundle exec rake db:import_temperature_records to import the CSV file
+# Run command: bundle exec rake db:import_temperature_records
+# to import the CSV file
 # (it will take a while to run)
 
 require 'csv'
